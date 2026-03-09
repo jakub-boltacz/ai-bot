3cdfa3802bea7caa152823a19f36c1435728b93b
Jakub Bolatcz <jakub.boltacz@gmail.com>
2026-03-09 22:41:13 +0100

    added token count

--- main.py
+++ main.py
@@ -7,8 +7,23 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise Exception("no api key")
 
+prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
 client = genai.Client(api_key=api_key)
-response = Client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-                                          
+response = client.models.generate_content(model='gemini-2.5-flash', contents= prompt)
+    
+usage=response.usage_metadata
+prompt_tokens = usage.prompt_token_count
+completion_tokens = usage.candidates_token_count
+totla_used = usage.total_token_count
+
+if  response.usage_metadata is None:
+    raise RuntimeError('no tokens used')
+else:
+    print(f'Prompt: {prompt}')
+    print(f'Response: {response.text}')
+    print(f'Prompt tokens: {prompt_tokens}')
+    print(f'Response tokens: {completion_tokens}')
+    print(f'Total tokens: {totla_used}')
+
 print(response.text)
 
