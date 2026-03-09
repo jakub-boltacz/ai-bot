55f3710d7907a3a1817c747f8b741a6803514958
Jakub Bolatcz <jakub.boltacz@gmail.com>
2026-03-09 23:15:03 +0100

    flexible prompt

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise Exception("no api key")
 
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model='gemini-2.5-flash', contents= prompt)
+response = client.models.generate_content(model='gemini-2.5-flash', contents= args.user_prompt)
     
 if  response.usage_metadata is None:
     raise RuntimeError('no tokens used')
@@ -20,7 +24,7 @@ completion_tokens = usage.candidates_token_count
 total_used = usage.total_token_count
 
 
-print(f'Prompt: {prompt}')
+print(f'Prompt: {args.user_prompt}')
 print("-" * 20)
 print(f'Response: {response.text}')
 print("-" * 20)
