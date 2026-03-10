df14eac820ee07d623476b0591d06ca307a2f15a
Jakub Bolatcz <jakub.boltacz@gmail.com>
2026-03-10 19:44:27 +0100

    verbose mode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,10 +11,12 @@ if api_key == None:
 
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(model='gemini-2.5-flash', contents= args.user_prompt)
+response = client.models.generate_content(model='gemini-2.5-flash', contents= messages)
     
 if  response.usage_metadata is None:
     raise RuntimeError('no tokens used')
@@ -24,12 +27,12 @@ completion_tokens = usage.candidates_token_count
 total_used = usage.total_token_count
 
 
-print(f'Prompt: {args.user_prompt}')
-print("-" * 20)
 print(f'Response: {response.text}')
-print("-" * 20)
-print(f'Prompt tokens: {prompt_tokens}')
-print(f'Response tokens: {completion_tokens}')
-print(f'Total tokens: {total_used}')
-
+if args.verbose:
+    print("-" * 20)
+    print(f'Prompt tokens: {prompt_tokens}')
+    print(f'Response tokens: {completion_tokens}')
+    print(f'Total tokens: {total_used}')
+    print("-" * 20)
+    print(f'Prompt: {args.user_prompt}')
 
